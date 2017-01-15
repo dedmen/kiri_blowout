21a12e7b75a03a0015ea453d1d86661b473b12ce
dedmen <dedmen@dedmen.de>
2017-01-15 18:06:31 +0100

    data directory

---
+++ addons/kiri_blowout_data/config.cpp
@@ -0,0 +1,12 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class kiri_blowout_data {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		author = "dedmen";
+		requiredAddons[] = {"kiri_blowout"};
+	};
+};
+
